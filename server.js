57a043fa65d44747738cc4e40ea96633e9a42298
gadbenshabat-lab <gad.benshabat@gmail.com>
2026-07-18 22:44:29 +0300

    fix index

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ app.use(express.json());
 
 app.use('/', adminRoutes);
 app.use(express.static('public'));
-
+app.get('/api/categories', (req, res) => {
+    const categories = [
+        { id: 1, name: "סלולר", image: "https://cdn-icons-png.flaticon.com/128/2965/2965879.png" },
+        { id: 2, name: "מקררים", image: "https://cdn-icons-png.flaticon.com/128/3002/3002621.png" },
+        { id: 3, name: "מחשבים", image: "https://cdn-icons-png.flaticon.com/128/3063/3063821.png" }
+    ];
+    res.json(categories);
+});
 app.post('/api/search', (req, res) => {
     const { category, brand } = req.body;
     const query = `
